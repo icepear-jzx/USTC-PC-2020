03a8f4b51b9c556358726bc48ab010db9bca2942
IcePear-Jzx <icepear.jzx@gmail.com>
2020-05-17 19:39:00 +0800

    finish lab2

--- Lab2/monte_carlo.c
+++ Lab2/monte_carlo.c
@@ -3,14 +3,18 @@
 #include <stdlib.h>
 #include <time.h>
 
+int car_v[1000000];
+int car_pos[1000000];
+int car_v_recv[1000000];
+int car_pos_recv[1000000];
+
 int main(int argc, char *argv[]) {
     clock_t start = clock();
     int car_num = atoi(argv[1]);
     int period = atoi(argv[2]);
     int v_max = atoi(argv[3]);
     float p = atof(argv[4]);
-    int car_v[1000000];
-    int car_pos[1000000];
+    int next_car_pos;
 
     MPI_Init(NULL, NULL);
 
@@ -20,66 +24,97 @@ int main(int argc, char *argv[]) {
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    int last_rank = (world_rank - 1 + world_size) % world_size;
-    int next_rank = (world_rank + 1) % world_size;
-    
-    // datatype
-    int valid_car_num;
-    valid_car_num = (car_num + world_size - 1) / world_size;
-    
-    MPI_Datatype intvector;
-    MPI_Type_vector(valid_car_num, 1, world_size, MPI_INT, &intvector);
-    MPI_Type_commit(&intvector);
+    int last_rank = world_rank - 1;
+    int next_rank = world_rank + 1;
 
+    int base_car_num = (car_num + world_size - 1) / world_size;
+    int last_car_num = car_num - base_car_num * (world_size - 1);
+    int valid_car_num;
+    if(next_rank == world_size)
+        valid_car_num = last_car_num;
+    else
+        valid_car_num = base_car_num;
+    
     // init
-    for(int car_i = world_rank; car_i < car_num; car_i += world_size) {
+    for(int car_i = 0; car_i < valid_car_num; car_i++) {
         car_v[car_i] = 0;
-        car_pos[car_i] = car_i;
+        car_pos[car_i] = base_car_num * world_rank + car_i;
     }
 
     // period 0
-    MPI_Send(car_pos + world_rank, 1, intvector, last_rank, 0, MPI_COMM_WORLD);
+    if(last_rank >= 0) {
+        MPI_Send(car_pos, 1, MPI_INT, last_rank, 0, MPI_COMM_WORLD);
+    }
 
     // period 0+
     for(int period_i = 1; period_i <= period; period_i++) {
-        printf("%d %d\n", world_rank, period_i);
-        MPI_Recv(car_pos + next_rank, 1, intvector, next_rank, period_i - 1, 
-            MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        for(int car_i = world_rank; car_i < car_num; car_i += world_size) {
+        // printf("%d %d\n", world_rank, period_i);
+        if(next_rank < world_size) {
+            MPI_Recv(&next_car_pos, 1, MPI_INT, next_rank, period_i - 1, 
+                MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        for(int car_i = 0; car_i < valid_car_num; car_i++) {
             int d;
-            if(car_i < car_num - 1) 
+            if(car_i < valid_car_num - 1) 
                 d = car_pos[car_i+1] - car_pos[car_i];
+            else if(next_rank == world_size)
+                d = v_max + 1;
+            else
+                d = next_car_pos - car_pos[car_i];
             // (2)
-            if(car_i == car_num - 1 || (d - 1) > car_v[car_i]) 
-                car_v[car_i]++;
+            if(car_num == base_car_num * world_rank + car_i || (d - 1) > car_v[car_i]) {
+                if(car_v[car_i] < v_max)
+                    car_v[car_i]++;
+            }
             // (3)
-            else if(d <= car_v[car_i])
+            else {
                 car_v[car_i] = d - 1;
+            }
             // (4)
             if(car_v[car_i] > 0 && (double)rand()/(double)RAND_MAX < p)
                 car_v[car_i] -= 1;
             // (5)
             car_pos[car_i] += car_v[car_i];
         }
-        MPI_Send(car_pos + world_rank, 1, intvector, last_rank, period_i, MPI_COMM_WORLD);
+        if(last_rank >= 0) {
+            MPI_Send(car_pos, 1, MPI_INT, last_rank, period_i, MPI_COMM_WORLD);
+        }
     }
 
     int tag_v = period + 1;
     int tag_pos = period + 2;
     if(world_rank == 0) {
-        printf("Road state: \n");
+        // printf("Road state: \n");
+        // for(int car_i = 0; car_i < valid_car_num; car_i++) {
+        //     printf("Car%d: v=%d pos=%d\n", car_i, 
+        //         car_v[car_i], car_pos[car_i]);
+        // }
         for(int rank = 1; rank < world_size; rank++) {
-            MPI_Recv(car_v + rank, 1, intvector, rank, tag_v, 
-                MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Recv(car_pos + rank, 1, intvector, rank, tag_pos, 
-                MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if(rank + 1 == world_size) {
+                MPI_Recv(car_v_recv, last_car_num, MPI_INT, rank, tag_v, 
+                    MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(car_pos_recv, last_car_num, MPI_INT, rank, tag_pos, 
+                    MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                // for(int car_i = 0; car_i < last_car_num; car_i++) {
+                //     printf("Car%d: v=%d pos=%d\n", base_car_num * rank + car_i, 
+                //         car_v_recv[car_i], car_pos_recv[car_i]);
+                // }
+            }
+            else {
+                MPI_Recv(car_v_recv, base_car_num, MPI_INT, rank, tag_v, 
+                    MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                MPI_Recv(car_pos_recv, base_car_num, MPI_INT, rank, tag_pos, 
+                    MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                // for(int car_i = 0; car_i < base_car_num; car_i++) {
+                //     printf("Car%d: v=%d pos=%d\n", base_car_num * rank + car_i, 
+                //         car_v_recv[car_i], car_pos_recv[car_i]);
+                // }
+            }
         }
-        for(int car_i = 0; car_i < car_num; car_i++) 
-            printf("Car%d: v=%d pos=%d\n", car_i, car_v[car_i], car_pos[car_i]);
         printf("Time: %f\n", (clock() - start) / (float)CLOCKS_PER_SEC);
     } else {
-        MPI_Send(car_v + world_rank, 1, intvector, 0, tag_v, MPI_COMM_WORLD);
-        MPI_Send(car_pos + world_rank, 1, intvector, 0, tag_pos, MPI_COMM_WORLD);
+        MPI_Send(car_v, valid_car_num, MPI_INT, 0, tag_v, MPI_COMM_WORLD);
+        MPI_Send(car_pos, valid_car_num, MPI_INT, 0, tag_pos, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
